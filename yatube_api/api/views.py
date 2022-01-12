2a82d60314fbaa409ff8b4f7883728eea1f2a32d
Lev Khalyapin <lev.kh@yandex.ru>
2022-01-12 12:13:03 +0300

    Second try

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,4 +1,4 @@
-from posts.models import Group, Post, Follow
+from posts.models import Group, Post
 from rest_framework import filters, permissions, viewsets, mixins
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
